f715868436b0a2f3627234cf1408612f38b7c639
Rohit4021 <87359774+Rohit4021@users.noreply.github.com>
2022-11-14 22:33:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const http = require('http') 
 const { Browser } = require('selenium-wrapper-ed')
 
 const automate = async () => {
     await Browser.openPage('https://app.resemble.ai')
 }
 
-automate()
+const server = http.createServer(function (req, res) {
+    http.createServer() 
+    res.send('Hi!') 
+    automate()
+}) 
+
+server.listen(process.env.PORT)  
